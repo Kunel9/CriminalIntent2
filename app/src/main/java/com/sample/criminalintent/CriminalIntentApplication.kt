75493304015a013c131a3cf5acf63f90c6963bfa
Diana <albinos55522@gmail.com>
2023-10-27 15:38:10 +0700

    Реализация запросов в DAO

---
+++ app/src/main/java/com/sample/criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,12 @@
+package com.sample.criminalintent
+
+import android.app.Application
+
+class CriminalIntentApplication: Application()
+{
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+
+}
